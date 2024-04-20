7ad769527425cd1be884ba01198145ee52e93293
Luis Felipe <lfelipe.desarrollador@gmail.com>
2024-04-20 00:56:43 -0500

    no whitenoise

--- angeluz/settings.py
+++ angeluz/settings.py
@@ -54,7 +54,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,7 +142,6 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'staticfiles/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "staticfiles/media")
