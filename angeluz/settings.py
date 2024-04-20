d2663c7ca64d1ec1cd14146c34b7d73cee1f02be
Luis Felipe <lfelipe.desarrollador@gmail.com>
2024-04-20 01:10:02 -0500

    media

--- angeluz/settings.py
+++ angeluz/settings.py
@@ -143,8 +143,8 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
-MEDIA_URL = 'staticfiles/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "staticfiles/media")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # CKEDITOR CONFIGURATION
 CKEDITOR_UPLOAD_PATH = 'uploads/'
