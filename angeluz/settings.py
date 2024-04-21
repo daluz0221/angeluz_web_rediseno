3c3b02d1b5a2f0d810af96944e00c6af018ee063
Luis Felipe <lfelipe.desarrollador@gmail.com>
2024-04-20 20:48:24 -0500

    cambios para images

--- angeluz/settings.py
+++ angeluz/settings.py
@@ -141,10 +141,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "media"),)
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # CKEDITOR CONFIGURATION
 CKEDITOR_UPLOAD_PATH = 'uploads/'
