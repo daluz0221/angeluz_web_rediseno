444884877885c4de2448f8f684fb41b61f840af3
Luis Felipe <lfelipe.desarrollador@gmail.com>
2024-04-21 01:10:19 -0500

    intento 10

--- angeluz/urls.py
+++ angeluz/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include   
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('grappelli/', include('grappelli.urls')),
@@ -30,5 +31,12 @@ urlpatterns = [
     path('newsletter/', include(('aplications.newsletter.urls', 'newsletter'), namespace='newsletter')),
     path('users/', include(('aplications.users.urls', 'users'), namespace='users')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
 
+
+
+urlpatterns += [
+    path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }
+]
